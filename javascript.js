6f9bb73b6e0f88cc20942371fe20984b0c4b0ab4
figueduff <figueduff@hotmail.com>
2022-09-21 10:41:07 -0300

    reconocer_operaciones

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let display = document.querySelector("#resultado");
 let btn = document.querySelectorAll(".mostrar_display");
+let btnEspecial = document.querySelectorAll(".especial")
+
 
 btn.forEach((btn) => {
   btn.addEventListener("click", e => {
@@ -7,8 +9,19 @@ btn.forEach((btn) => {
   });
 });
 
+
+btnEspecial.forEach((btnEspecial) => {
+    btnEspecial.addEventListener("click",e => {
+        aplicarOperacion()
+    })
+})
+
+
 function agregarCaracterDisplay(dato) {
   if (display.textContent == null || display.textContent == "0")
     display.textContent = dato;
   else display.textContent += dato;
 }
+
+function aplicarOperacion(dato)
+{}
